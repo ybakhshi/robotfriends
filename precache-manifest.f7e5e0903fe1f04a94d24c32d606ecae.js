98892d94238df31e9043d8aa1a12ceab8657aed9
Yonus Bakhshi <yonus.bakhshi@gmail.com>
2019-10-28 01:07:16 +0100

    Updates

---
+++ precache-manifest.f7e5e0903fe1f04a94d24c32d606ecae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957e409b0efcbd82d29c0d51af62366d",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "6746344d30eb2f92f34e",
+    "url": "/robotfriends/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "97b404a62590646e72f2",
+    "url": "/robotfriends/static/css/main.76ef0e75.chunk.css"
+  },
+  {
+    "revision": "6746344d30eb2f92f34e",
+    "url": "/robotfriends/static/js/2.33cbe468.chunk.js"
+  },
+  {
+    "revision": "97b404a62590646e72f2",
+    "url": "/robotfriends/static/js/main.5dea0f7f.chunk.js"
+  },
+  {
+    "revision": "d9953065c9c3306ad6b5",
+    "url": "/robotfriends/static/js/runtime-main.08a6fd18.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
